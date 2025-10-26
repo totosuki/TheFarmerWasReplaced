4cd39953796b4409b96444a844ca8d86b300de84
Totosuki <e231kari@gmail.com>
2025-10-26 21:05:38 +0900

    Fix tree.py

--- _251022_tree.py
+++ _251022_tree.py
@@ -5,53 +5,53 @@ CARROT = 6
 WATER_LINE = 0.25
 
 def get_grass():
-    harvest()
+	harvest()
 
 def get_tree():
-    harvest()
-    plant(Entities.Tree)
+	harvest()
+	plant(Entities.Tree)
 
 def get_carrot():
-    harvest()
-    plant(Entities.Carrot)
+	harvest()
+	plant(Entities.Carrot)
 
 def watering():
-    if get_water() <= WATER_LINE:
-        use_item(Items.Water)
+	if get_water() <= WATER_LINE:
+		use_item(Items.Water)
 
 def main():
-    cnt = 0
-    for row in range(SIZE):
-        for col in range(SIZE):
-            cnt += 1
+	cnt = 0
+	for row in range(SIZE):
+		for col in range(SIZE):
+			cnt += 1
 
-            while not can_harvest():
-                pass
+			while not can_harvest():
+				pass
 
-            watering()
+			watering()
 
-            if row < TREE and (cnt - row % 2) % 2:
-                get_grass()
-            elif row < TREE:
-                get_tree()
-            else:
-                get_carrot()
-            
-            move(East)
-        move(South)
+			if row < TREE and (cnt - row % 2) % 2:
+				get_grass()
+			elif row < TREE:
+				get_tree()
+			else:
+				get_carrot()
+			
+			move(East)
+		move(South)
 
 def initialize():
-    clear()
-    
-    for _ in range(SIZE - 1):
-        move(North)
-    
-    for row in range(SIZE):
-        for col in range(SIZE):
-            if row < TREE:
-                continue
-            else:
-                till()
-                plant(Entities.Carrot)
-            move(East)
-        move(South)
+	clear()
+	
+	for _ in range(SIZE - 1):
+		move(North)
+	
+	for row in range(SIZE):
+		for col in range(SIZE):
+			if row < TREE:
+				continue
+			else:
+				till()
+				plant(Entities.Carrot)
+			move(East)
+		move(South)
